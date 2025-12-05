4e7a6d245fb4b694a1c74d9f63e26bb55388c804
Paisena <m8ames@gmail.com>
2025-12-04 20:23:48 -0800

    Reverted to other repository
    I got a new computer and the CMake was giving me issues so I had to remake the program in another repo

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/SchoolStuff/UCSC/UCSC Year 3 FALL/CMPM 123/Code/Chess-AI
+# Build directory: C:/SchoolStuff/UCSC/UCSC Year 3 FALL/CMPM 123/Code/Chess-AI/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
